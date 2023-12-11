c54f38b361120c090148adcb57a5528f6b14d1e8
prajjwal112 <prajjwal200491@gmail.com>
2023-12-11 22:53:36 +0530

    updated env

--- HanziAnimationUI/src/environments/environment.prod.ts
+++ HanziAnimationUI/src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
-  firebase: {
-    projectId: 'fir-test-application-d5087',
-    appId: '1:870695196830:web:ed01afd08e2c673c21c38f',
-    databaseURL: 'https://fir-test-application-d5087-default-rtdb.firebaseio.com',
-    storageBucket: 'fir-test-application-d5087.appspot.com',
-    apiKey: 'AIzaSyBEJL_ZZhpdrQVmoy-zygUMl-033NswFYI',
-    authDomain: 'fir-test-application-d5087.firebaseapp.com',
-    messagingSenderId: '870695196830',
-    measurementId: 'G-MCZ27VPMQD',
-  },
+  // firebase: {
+  //   projectId: 'fir-test-application-d5087',
+  //   appId: '1:870695196830:web:ed01afd08e2c673c21c38f',
+  //   databaseURL: 'https://fir-test-application-d5087-default-rtdb.firebaseio.com',
+  //   storageBucket: 'fir-test-application-d5087.appspot.com',
+  //   apiKey: 'AIzaSyBEJL_ZZhpdrQVmoy-zygUMl-033NswFYI',
+  //   authDomain: 'fir-test-application-d5087.firebaseapp.com',
+  //   messagingSenderId: '870695196830',
+  //   measurementId: 'G-MCZ27VPMQD',
+  // },
+  apiUrl:'https://chinese-animator-webapp.azurewebsites.net',
   production: true
 };
