5a313d64f0784d8e6f35e5208fab69ed9c4fd8e3
prajjwal112 <prajjwal200491@gmail.com>
2023-12-11 22:09:29 +0530

    process

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-const port =3000;
+const port = process.env.PORT||3000;
 const {poolPromise} = require('./db');
 app.use(cors());
 const addLists = require('./addListWithCard&Characters');
